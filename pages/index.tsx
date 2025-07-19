df0dc189df0623352dc178cc651f65454df02ccc
HIROYOKOKI <h.yokoki@icloud.com>
2025-07-20 00:53:10 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="p-8 text-center bg-gray-50 min-h-screen">
-      <h1 className="text-4xl font-bold text-green-600 animate-fadeInUp mb-4">
-        🌱 やっと出た！！！！！！！！！
+      <h1 className="text-3xl font-bold text-green-500 animate-fadeInUp mb-4">
+        🌱 やっと出た！！！！！！
       </h1>
       <p className="text-lg text-gray-700 animate-fadeInUp delay-[200ms]">
         アニメーションが効いてます。
