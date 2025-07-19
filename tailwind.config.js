2c25f5fb09322b12b29b6d51c279672ca5008e22
HIROYOKOKI <h.yokoki@icloud.com>
2025-07-19 20:30:28 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-  './pages/**/*.{js,ts,jsx,tsx}',
-  './components/**/*.{js,ts,jsx,tsx}',
-  './app/**/*.{js,ts,jsx,tsx}',
-  './styles/**/*.{css}',             // ✅ ← これが必要！
-],
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.css', // ✅ これが必要！
+  ],
   theme: {
     extend: {
       colors: {
@@ -18,4 +18,8 @@ module.exports = {
     },
   },
   plugins: [],
-}
+  future: {
+    respectDefaultRingColorOpacity: false,
+    respectDefaultOpacity: false,
+  },
+};
