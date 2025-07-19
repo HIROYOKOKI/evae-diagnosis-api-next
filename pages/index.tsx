80d2ec4d878c42583d6b7c07af0088053a1be261
HIROYOKOKI <h.yokoki@icloud.com>
2025-07-19 23:47:24 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-// ファイル: pages/index.tsx
 export default function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold text-green-500">
-        🌱 やっと出た！！！！！！！！！
+    <div className="p-8 text-center">
+      <h1 className="text-4xl font-bold text-green-600">
+        🌟 出た！！！！！！！！！
       </h1>
+      <p className="mt-4 text-xl text-gray-700">
+        ついにあなたのNext.jsアプリが表示されました。
+      </p>
     </div>
   );
 }
