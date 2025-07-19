0d23a9ad61d8e62fa30a86213351e78cb33c78b1
HIROYOKOKI <h.yokoki@icloud.com>
2025-07-19 23:09:23 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-// ファイル: pages/index.tsx
+// pages/index.tsx
 export default function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold text-green-600">🌱 出た！最小構成OK！</h1>
+    <div>
+      <p>✅ 出た！最小ビルド成功！</p>
     </div>
   );
 }
