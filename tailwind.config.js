fd0d6e3a0488a16ba093311b697355e37a98a254
HIROYOKOKI <h.yokoki@icloud.com>
2025-07-20 20:19:41 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,16 @@ module.exports = {
     './styles/**/*.{css}',
   ],
   safelist: [
-    'animate-fadeInUp',
-    'delay-[200ms]',
-    'delay-[400ms]',
-    'text-green-500',
-    'text-3xl',
-    'font-bold',
-  ], // ← ✅ カンマ追加
+  {
+    pattern: /animate-.*/,
+  },
+  {
+    pattern: /delay-.*/,
+  },
+  'text-green-500',
+  'text-3xl',
+  'font-bold',
+], // ← ✅ カンマ追加
   theme: {
     extend: {
       colors: {
