db430a039031eab8565da5c962eca451aa5b91fd
HIROYOKOKI <h.yokoki@icloud.com>
2025-07-30 17:21:46 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 // pages/_app.tsx
-import '../styles/globals.css';
+import '../styles/globals.css'; // ここでglobals.cssをインポート
 
-function MyApp({ Component, pageProps }) {
+import type { AppProps } from 'next/app'; // AppPropsの型定義をインポート
+
+// アプリケーションのルートコンポーネント
+// export default はこの関数に対して一度だけ記述します。
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+export default MyApp; // MyAppをデフォルトエクスポート
